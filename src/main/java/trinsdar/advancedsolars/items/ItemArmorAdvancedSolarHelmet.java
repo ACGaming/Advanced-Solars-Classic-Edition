bb84c4c076286f2da8328bb226002c3e682e0e8b
ACGaming <4818419+ACGaming@users.noreply.github.com>
2024-04-23 16:37:16 +0200

    Inherit all QuantumSuit attributes, fix upgrades
    Trins-mods/Advanced-Solars-Classic-Edition#8

--- src/main/java/trinsdar/advancedsolars/items/ItemArmorAdvancedSolarHelmet.java
+++ src/main/java/trinsdar/advancedsolars/items/ItemArmorAdvancedSolarHelmet.java
@@ -1,21 +1,39 @@
 package trinsdar.advancedsolars.items;
 
+import ic2.api.classic.item.ICropAnalyzer;
+import ic2.api.classic.item.IEUReader;
+import ic2.api.classic.item.IThermometer;
 import ic2.api.item.ElectricItem;
+import ic2.api.item.IMetalArmor;
 import ic2.core.IC2;
 import ic2.core.block.generator.tile.TileEntitySolarPanel;
+import ic2.core.entity.IC2Potion;
 import ic2.core.inventory.base.IHasInventory;
+import ic2.core.inventory.transport.IItemTransporter;
+import ic2.core.inventory.transport.TransporterManager;
 import ic2.core.item.armor.base.ItemElectricArmorBase;
+import ic2.core.item.misc.ItemTinCan;
+import ic2.core.platform.lang.storage.Ic2InfoLang;
+import ic2.core.platform.registry.Ic2Lang;
 import ic2.core.platform.textures.Ic2Icons;
-import ic2.core.util.obj.IBootable;
+import ic2.core.platform.textures.obj.IColorEffectedTexture;
+import ic2.core.util.misc.StackUtil;
 import ic2.core.util.obj.ToolTipType;
 import ic2.core.util.obj.plugins.IBaublesPlugin;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.NonNullList;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ISpecialArmor;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import trinsdar.advancedsolars.AdvancedSolarsClassic;
@@ -25,17 +43,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-public class ItemArmorAdvancedSolarHelmet extends ItemElectricArmorBase implements IBootable {
-    @Override
-    public void onLoad() {
-
-    }
-
-    private int
-    id,
-    production,
-    lowerProduction,
-    energyPerDamage;
+public class ItemArmorAdvancedSolarHelmet extends ItemElectricArmorBase implements IMetalArmor, IColorEffectedTexture, IEUReader, IThermometer, ICropAnalyzer {
+    private final int id;
+    private final int production;
+    private final int lowerProduction;
+    private final int energyPerDamage;
     double damageAbsorpationRatio;
     String texture;
 
@@ -56,20 +68,68 @@ public class ItemArmorAdvancedSolarHelmet extends ItemElectricArmorBase implemen
         super.onSortedItemToolTip(stack, player, debugTooltip, tooltip, sortedTooltip);
         sortedTooltip.get(ToolTipType.Shift).add(AdvancedSolarLang.helmetProduction.getLocalizedFormatted(production));
         sortedTooltip.get(ToolTipType.Shift).add(AdvancedSolarLang.helmetLowerPorduction.getLocalizedFormatted(lowerProduction));
-
+        sortedTooltip.get(ToolTipType.Shift).add(Ic2InfoLang.qArmorFeedsYou.getLocalized());
+        sortedTooltip.get(ToolTipType.Shift).add(Ic2InfoLang.qArmorRemovePotions.getLocalized());
+        sortedTooltip.get(ToolTipType.Shift).add(Ic2InfoLang.qArmorGivesAir.getLocalized());
+        NBTTagCompound nbt = StackUtil.getNbtData(stack);
+        if (nbt.hasKey("EUReaderUpgrade")) {
+            sortedTooltip.get(ToolTipType.Shift).add(Ic2Lang.upgradeEU.getLocalized());
+        }
+        if (nbt.hasKey("CropUpgrade")) {
+            sortedTooltip.get(ToolTipType.Shift).add(Ic2Lang.upgradeCrop.getLocalized());
+        }
+        if (nbt.hasKey("ThermometerUpgrade")) {
+            sortedTooltip.get(ToolTipType.Shift).add(Ic2Lang.upgradeThermo.getLocalized());
+        }
     }
 
     @Override
     public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack) {
-        if (!IC2.platform.isRendering()) {
-            if (world.provider.hasSkyLight() && world.canSeeSky(player.getPosition())){
-                if (TileEntitySolarPanel.isSunVisible(world, player.getPosition())) {
-                    chargeInventory(player, production, tier, itemStack);
-                }else {
-                    chargeInventory(player, lowerProduction, tier, itemStack);
+        if (!IC2.platform.isRendering() && world.provider.hasSkyLight() && world.canSeeSky(player.getPosition())) {
+            if (TileEntitySolarPanel.isSunVisible(world, player.getPosition())) {
+                chargeInventory(player, production, tier, itemStack);
+            } else {
+                chargeInventory(player, lowerProduction, tier, itemStack);
+            }
+        }
+
+        boolean server = IC2.platform.isSimulating();
+        if (server) {
+            int air = player.getAir();
+            if (air < 100 && this.canUseEnergy(itemStack, 1000.0)) {
+                player.setAir(air + 200);
+                this.useEnergy(itemStack, 1000.0, player);
+            }
+
+            if (player.getFoodStats().needFood() && this.canUseEnergy(itemStack, 1000.0)) {
+                IItemTransporter trans = TransporterManager.manager.getTransporter(player, true);
+                if (trans != null) {
+                    ItemStack result = trans.removeItem(ItemTinCan.foodCanFilter, EnumFacing.DOWN, 1, true);
+                    if (!result.isEmpty()) {
+                        ItemTinCan can = (ItemTinCan) result.getItem();
+                        can.onFoodEaten(result, world, player);
+                        this.useEnergy(itemStack, 1000.0, player);
+                    }
                 }
             }
 
+            PotionEffect poison = player.getActivePotionEffect(MobEffects.POISON);
+            if (poison != null && this.canUseEnergy(itemStack, 10000 * poison.getAmplifier())) {
+                this.useEnergy(itemStack, 10000 * poison.getAmplifier(), player);
+                IC2.platform.removePotion(player, MobEffects.POISON);
+            }
+
+            PotionEffect radiation = player.getActivePotionEffect(IC2Potion.radiation);
+            if (radiation != null && this.canUseEnergy(itemStack, 20000 * radiation.getAmplifier())) {
+                this.useEnergy(itemStack, 20000 * radiation.getAmplifier(), player);
+                IC2.platform.removePotion(player, IC2Potion.radiation);
+            }
+
+            PotionEffect wither = player.getActivePotionEffect(MobEffects.WITHER);
+            if (wither != null && this.canUseEnergy(itemStack, 25000 * wither.getAmplifier())) {
+                this.useEnergy(itemStack, 25000 * wither.getAmplifier(), player);
+                IC2.platform.removePotion(player, MobEffects.WITHER);
+            }
         }
     }
 
@@ -105,20 +165,19 @@ public class ItemArmorAdvancedSolarHelmet extends ItemElectricArmorBase implemen
     }
 
     public int chargeInventory(EntityPlayer player, int provided, int tier, ItemStack helmet) {
-
         int i;
         List<NonNullList<ItemStack>> invList = Arrays.asList(player.inventory.armorInventory, player.inventory.offHandInventory, player.inventory.mainInventory);
 
-        if (ElectricItem.manager.getCharge(helmet) != ElectricItem.manager.getMaxCharge(helmet)){
-            int charged = (int)(ElectricItem.manager.charge(helmet, (double)provided, this.tier, false, false));
+        if (ElectricItem.manager.getCharge(helmet) != ElectricItem.manager.getMaxCharge(helmet)) {
+            int charged = (int) (ElectricItem.manager.charge(helmet, provided, this.tier, false, false));
             provided -= charged;
         } else {
             for (NonNullList inventory : invList) {
                 int inventorySize = inventory.size();
-                for (i=0; i < inventorySize && provided > 0; i++) {
-                    ItemStack tStack = (ItemStack)inventory.get(i);
+                for (i = 0; i < inventorySize && provided > 0; i++) {
+                    ItemStack tStack = (ItemStack) inventory.get(i);
                     if (tStack.isEmpty()) continue;
-                    int charged = (int)(ElectricItem.manager.charge(tStack, (double)provided, this.tier, false, false));
+                    int charged = (int) (ElectricItem.manager.charge(tStack, provided, this.tier, false, false));
                     provided -= charged;
                 }
             }
@@ -127,11 +186,11 @@ public class ItemArmorAdvancedSolarHelmet extends ItemElectricArmorBase implemen
             if (plugin != null) {
                 IHasInventory inv = plugin.getBaublesInventory(player);
 
-                for(i = 0; i < inv.getSlotCount(); ++i) {
+                for (i = 0; i < inv.getSlotCount(); ++i) {
                     if (provided <= 0) {
                         break;
                     }
-                    provided = (int)((double)provided - ElectricItem.manager.charge(inv.getStackInSlot(i), (double)provided, tier, false, false));
+                    provided = (int) (provided - ElectricItem.manager.charge(inv.getStackInSlot(i), provided, tier, false, false));
                 }
             }
         }
@@ -150,6 +209,59 @@ public class ItemArmorAdvancedSolarHelmet extends ItemElectricArmorBase implemen
                 return EnumRarity.EPIC;
             default:
                 return super.getRarity(stack);
+        }
+    }
+
+    public boolean isCropAnalyzer(ItemStack stack) {
+        return StackUtil.getNbtData(stack).getBoolean("CropUpgrade");
+    }
+
+    public boolean isThermometer(ItemStack stack) {
+        return StackUtil.getNbtData(stack).getBoolean("ThermometerUpgrade");
+    }
+
+    public boolean isEUReader(ItemStack stack) {
+        return StackUtil.getNbtData(stack).getBoolean("EUReaderUpgrade");
+    }
+
+    @Override
+    public boolean showDurabilityBar(ItemStack stack) {
+        return !StackUtil.getNbtData(stack).getBoolean("HideDurability");
+    }
+
+    @Override
+    public boolean hasColor(ItemStack stack) {
+        NBTTagCompound nbt = stack.getSubCompound("display");
+        return nbt != null && nbt.hasKey("color");
+    }
+
+    @Override
+    public boolean hasOverlay(ItemStack stack) {
+        return this.hasColor(stack);
+    }
+
+    @Override
+    public void setColor(ItemStack stack, int color) {
+        stack.getOrCreateSubCompound("display").setInteger("color", color);
+    }
+
+    @Override
+    public int getColor(ItemStack stack) {
+        return StackUtil.getNbtData(stack).getCompoundTag("display").getInteger("color");
+    }
+
+    public boolean isMetalArmor(ItemStack itemstack, EntityPlayer player) {
+        return true;
+    }
+
+    @Override
+    public ISpecialArmor.ArmorProperties getProperties(EntityLivingBase player, ItemStack armor, DamageSource source, double damage, int slot) {
+        if (source == DamageSource.FALL && this.armorType == EntityEquipmentSlot.FEET) {
+            int energyPerDamage = this.getEnergyPerDamage();
+            int damageLimit = (int) (energyPerDamage > 0 ? ElectricItem.manager.discharge(armor, 2.147483647E9, Integer.MAX_VALUE, true, false, true) / energyPerDamage : 0.0);
+            return new ISpecialArmor.ArmorProperties(10, IC2.config.getFloat("electricSuitAbsorbtionScale"), damageLimit);
+        } else {
+            return super.getProperties(player, armor, source, damage, slot);
         }
     }
 }
--- src/main/java/trinsdar/advancedsolars/util/AdvancedSolarsRecipes.java
+++ src/main/java/trinsdar/advancedsolars/util/AdvancedSolarsRecipes.java
@@ -6,25 +6,26 @@ import ic2.api.recipe.IRecipeInput;
 import ic2.core.block.machine.low.TileEntityCompressor;
 import ic2.core.item.recipe.entry.RecipeInputItemStack;
 import ic2.core.item.recipe.entry.RecipeInputOreDict;
+import ic2.core.item.recipe.upgrades.FlagModifier;
 import ic2.core.platform.registry.Ic2Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.oredict.OreDictionary;
 
 public class AdvancedSolarsRecipes {
-    public static void init(){
+    static ICraftingRecipeList recipes = ClassicRecipes.advCrafting;
+
+    public static void init() {
         initCraftingRecipes();
         initMiscRecipes();
     }
 
-    static ICraftingRecipeList recipes = ClassicRecipes.advCrafting;
-
-    static IRecipeInput getPlasma(){
+    static IRecipeInput getPlasma() {
         return Loader.isModLoaded("gtclassic") ? new RecipeInputOreDict("itemPlasma") : new RecipeInputItemStack(Ic2Items.plasmaCell);
     }
 
-    public static void initCraftingRecipes(){
-        if (AdvancedSolarsConfig.enabledItems.enableMiscCraftingItems){
+    public static void initCraftingRecipes() {
+        if (AdvancedSolarsConfig.enabledItems.enableMiscCraftingItems) {
             recipes.addRecipe(new ItemStack(Registry.sunnarium), "uuu", "ggg", "uuu", 'u', Ic2Items.uuMatter, 'g', "dustGlowstone");
             recipes.addRecipe(new ItemStack(Registry.sunnariumAlloy), "iii", "isi", "iii", 'i', Ic2Items.iridiumPlate, 's', Registry.sunnarium);
             recipes.addRecipe(new ItemStack(Registry.iridiumIronPlate), "rrr", "rir", "rrr", 'r', "ingotRefinedIron", 'i', "ingotIridium");
@@ -34,45 +35,62 @@ public class AdvancedSolarsRecipes {
             recipes.addRecipe(new ItemStack(Registry.irradiantUranium), "ugu", "geg", "ugu", 'u', Ic2Items.uuMatter, 'e', getUranium(), 'g', "dustGlowstone");
             recipes.addRecipe(new ItemStack(Registry.enrichedSunnarium), "iii", "isi", "iii", 'i', Registry.irradiantUranium, 's', Registry.sunnarium);
             recipes.addRecipe(new ItemStack(Registry.irradiantGlassPane, 6), "rrr", "igi", "rrr", 'r', Ic2Items.reinforcedGlass, 'i', Registry.irradiantUranium, 'g', "dustGlowstone");
-            if (AdvancedSolarsConfig.enabledItems.enableAdvancedSolarPanel){
+            if (AdvancedSolarsConfig.enabledItems.enableAdvancedSolarPanel) {
                 recipes.addRecipe(new ItemStack(Registry.advancedSolarPanel), "rrr", "asa", "cic", 'r', Registry.irradiantGlassPane, 'a', Ic2Items.advancedAlloy, 's', Ic2Items.lvSolarPanel, 'c', "circuitAdvanced", 'i', Registry.irradiantReinforcedPlate);
-                if (AdvancedSolarsConfig.enabledItems.enableAdvancedSolarHelmet){
+                if (AdvancedSolarsConfig.enabledItems.enableAdvancedSolarHelmet) {
                     recipes.addRecipe(new ItemStack(Registry.advancedSolarHelmet), "sas", "cnc", "glg", 's', Ic2Items.advSolarHelmet, 'a', Registry.advancedSolarPanel, 'c', "circuitAdvanced", 'n', Ic2Items.nanoHelmet, 'g', Ic2Items.doubleInsulatedGoldCable, 'l', Ic2Items.transformerLV);
                 }
-                if (AdvancedSolarsConfig.enabledItems.enableHybridSolarPanel){
+                if (AdvancedSolarsConfig.enabledItems.enableHybridSolarPanel) {
                     recipes.addRecipe(new ItemStack(Registry.hybridSolarPanel), "CmC", "iai", "csc", 'C', Ic2Items.carbonPlate, 'm', Ic2Items.mvSolarPanel, 'i', Ic2Items.iridiumPlate, 'a', Registry.advancedSolarPanel, 'c', "circuitAdvanced", 's', Registry.enrichedSunnarium);
                 }
             }
-            if (AdvancedSolarsConfig.enabledItems.enableHybridSolarPanel){
-                if (AdvancedSolarsConfig.enabledItems.enableHybridSolarHelmet){
+            if (AdvancedSolarsConfig.enabledItems.enableHybridSolarPanel) {
+                if (AdvancedSolarsConfig.enabledItems.enableHybridSolarHelmet) {
                     recipes.addRecipe(new ItemStack(Registry.hybridSolarHelmet), " H ", "cqc", "ghg", 'H', Registry.hybridSolarPanel, 'c', "circuitAdvanced", 'q', Ic2Items.quantumHelmet, 'g', Ic2Items.glassFiberCable, 'h', Ic2Items.transformerHV);
                 }
-                if (AdvancedSolarsConfig.enabledItems.enableUltimateHybridSolarPanel){
+                if (AdvancedSolarsConfig.enabledItems.enableUltimateHybridSolarPanel) {
                     recipes.addRecipe(new ItemStack(Registry.ultimateHybridSolarPanel), " h ", "pHp", "scs", 'h', Ic2Items.hvSolarPanel, 'p', Ic2Items.plasmaCore, 'H', Registry.hybridSolarPanel, 's', Registry.enrichedSunnariumAlloy, 'c', Ic2Items.coalChunk);
                 }
             }
-            if (AdvancedSolarsConfig.enabledItems.enableHybridSolarHelmet && AdvancedSolarsConfig.enabledItems.enableUltimateHybridSolarPanel && AdvancedSolarsConfig.enabledItems.enableUltimateHybridSolarHelmet){
-                recipes.addRecipe(new ItemStack(Registry.ultimateHybridSolarHelmet),  "ppp", "pup", "ihi", 'p', getPlasma(), 'u', Registry.ultimateHybridSolarPanel, 'i', Ic2Items.iridiumPlate, 'h', Registry.hybridSolarHelmet);
+            if (AdvancedSolarsConfig.enabledItems.enableHybridSolarHelmet && AdvancedSolarsConfig.enabledItems.enableUltimateHybridSolarPanel && AdvancedSolarsConfig.enabledItems.enableUltimateHybridSolarHelmet) {
+                recipes.addRecipe(new ItemStack(Registry.ultimateHybridSolarHelmet), "ppp", "pup", "ihi", 'p', getPlasma(), 'u', Registry.ultimateHybridSolarPanel, 'i', Ic2Items.iridiumPlate, 'h', Registry.hybridSolarHelmet);
             }
             recipes.addRecipe(new ItemStack(Registry.enrichedSunnariumAlloy), "pep", "ese", "pep", 'p', getPlasma(), 'e', Registry.enrichedSunnarium, 's', Registry.sunnariumAlloy);
 
-        }
+            ItemStack advancedSolarHelmet = new ItemStack(Registry.advancedSolarHelmet);
+            recipes.addShapelessRecipe(advancedSolarHelmet.copy(), (new FlagModifier(advancedSolarHelmet.copy(), "EUReaderUpgrade", true)).setUsesInput(), advancedSolarHelmet.copy(), Ic2Items.euReader.copy());
+            recipes.addShapelessRecipe(advancedSolarHelmet.copy(), (new FlagModifier(advancedSolarHelmet.copy(), "CropUpgrade", true)).setUsesInput(), advancedSolarHelmet.copy(), Ic2Items.cropAnalyzer.copy());
+            recipes.addShapelessRecipe(advancedSolarHelmet.copy(), (new FlagModifier(advancedSolarHelmet.copy(), "ThermometerUpgrade", true)).setUsesInput(), advancedSolarHelmet.copy(), Ic2Items.thermometer.copy());
 
+            ItemStack hybridSolarHelmet = new ItemStack(Registry.hybridSolarHelmet);
+            recipes.addShapelessRecipe(hybridSolarHelmet.copy(), (new FlagModifier(hybridSolarHelmet.copy(), "EUReaderUpgrade", true)).setUsesInput(), hybridSolarHelmet.copy(), Ic2Items.euReader.copy());
+            recipes.addShapelessRecipe(hybridSolarHelmet.copy(), (new FlagModifier(hybridSolarHelmet.copy(), "CropUpgrade", true)).setUsesInput(), hybridSolarHelmet.copy(), Ic2Items.cropAnalyzer.copy());
+            recipes.addShapelessRecipe(hybridSolarHelmet.copy(), (new FlagModifier(hybridSolarHelmet.copy(), "ThermometerUpgrade", true)).setUsesInput(), hybridSolarHelmet.copy(), Ic2Items.thermometer.copy());
+
+            ItemStack ultimateHybridSolarHelmet = new ItemStack(Registry.ultimateHybridSolarHelmet);
+            recipes.addShapelessRecipe(ultimateHybridSolarHelmet.copy(), (new FlagModifier(ultimateHybridSolarHelmet.copy(), "EUReaderUpgrade", true)).setUsesInput(), ultimateHybridSolarHelmet.copy(), Ic2Items.euReader.copy());
+            recipes.addShapelessRecipe(ultimateHybridSolarHelmet.copy(), (new FlagModifier(ultimateHybridSolarHelmet.copy(), "CropUpgrade", true)).setUsesInput(), ultimateHybridSolarHelmet.copy(), Ic2Items.cropAnalyzer.copy());
+            recipes.addShapelessRecipe(ultimateHybridSolarHelmet.copy(), (new FlagModifier(ultimateHybridSolarHelmet.copy(), "ThermometerUpgrade", true)).setUsesInput(), ultimateHybridSolarHelmet.copy(), Ic2Items.thermometer.copy());
+        }
     }
 
-    public static IRecipeInput getUranium(){
-        IRecipeInput defaultUranium =  new RecipeInputItemStack(Ic2Items.enderPearlUraniumIngot.copy());
-        switch (AdvancedSolarsConfig.misc.ingotInIrradiantUranium){
-            case URANIUM: return new RecipeInputOreDict("ingotUranium");
-            case URANIUM233: return OreDictionary.doesOreNameExist("ingotUranium233") ? new RecipeInputOreDict("ingotUranium233") : defaultUranium;
-            case URANIUM235: return OreDictionary.doesOreNameExist("ingotUranium235") ? new RecipeInputOreDict("ingotUranium235") : defaultUranium;
-            default: return defaultUranium;
+    public static IRecipeInput getUranium() {
+        IRecipeInput defaultUranium = new RecipeInputItemStack(Ic2Items.enderPearlUraniumIngot.copy());
+        switch (AdvancedSolarsConfig.misc.ingotInIrradiantUranium) {
+            case URANIUM:
+                return new RecipeInputOreDict("ingotUranium");
+            case URANIUM233:
+                return OreDictionary.doesOreNameExist("ingotUranium233") ? new RecipeInputOreDict("ingotUranium233") : defaultUranium;
+            case URANIUM235:
+                return OreDictionary.doesOreNameExist("ingotUranium235") ? new RecipeInputOreDict("ingotUranium235") : defaultUranium;
+            default:
+                return defaultUranium;
         }
     }
 
-    public static void initMiscRecipes(){
+    public static void initMiscRecipes() {
         OreDictionary.registerOre("ingotIridium", Registry.iridiumIngot);
-        if (!Loader.isModLoaded("gtclassic") && !Loader.isModLoaded("techreborn")){
+        if (!Loader.isModLoaded("gtclassic") && !Loader.isModLoaded("techreborn")) {
             TileEntityCompressor.addRecipe(Ic2Items.iridiumOre, new ItemStack(Registry.iridiumIngot));
         }
     }
